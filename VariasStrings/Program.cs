f739f005be7e7878519f9272d640e92f6552b523
Ricardo Louro <ricardo.f.louro@gmail.com>
2023-03-02 23:26:30 +0000

    Add Exercise 4

---
+++ VariasStrings/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace VariasStrings
+{
+    ///What tags?
+    class Program
+    {
+        ///A lil bit confused
+        static void Main(string[] args)
+        {
+            int x = 24;
+            char y = '\u00A9';
+            string a = "Hello, I'm Bugs";
+            string b = $"I'm {x} years old!";
+            string c = "I'm {x} years old!";
+            string d = @"Hope you guys have a good one! ^__^";
+            string e = $"Cool character, {y}";
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+            Console.WriteLine(e);
+        }
+    }
+}
