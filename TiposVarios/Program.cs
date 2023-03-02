c2a94a6354e5113b911e14a56b3a77248b6e79c6
Ricardo Louro <ricardo.f.louro@gmail.com>
2023-03-02 23:37:53 +0000

    Add TiposVarios with int, byte and short

---
+++ TiposVarios/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TiposVarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 40000;
+            byte b = 255;
+            short c = -32768;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+
+        }
+    }
+}
