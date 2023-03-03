ab647ea90aa0a9b0dc6508e211e1418fcd4aedd5
Ricardo Louro <ricardo.f.louro@gmail.com>
2023-03-03 00:00:10 +0000

    Add Exercicio 5

--- VariasStrings/Program.cs
+++ VariasStrings/Program.cs
@@ -16,11 +16,23 @@ namespace VariasStrings
             string d = @"Hope you guys have a good one! ^__^";
             string e = $"Cool character, {y}";
 
+            string f = "a" + 2;
+            string g = "abc" + f;
+            string h = $"O valor de g é {g}";
+            string i = String.Format("F é {1} e H é {0}", h,f);
+
             Console.WriteLine(a);
             Console.WriteLine(b);
             Console.WriteLine(c);
             Console.WriteLine(d);
             Console.WriteLine(e);
+
+            Console.WriteLine(f);
+            Console.WriteLine(g);
+            Console.WriteLine(h);
+            Console.WriteLine(i);
+
+            Console.WriteLine(@"Verbatim com f = {0}", f);
         }
     }
 }
